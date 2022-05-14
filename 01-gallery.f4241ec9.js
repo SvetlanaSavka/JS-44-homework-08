d4604ed804a5a35e22766b41144c26fd371bac1e
SvetlanaSavka <93277750+SvetlanaSavka@users.noreply.github.com>
2022-05-14 10:52:58 +0000

    Deploying to gh-pages from @ SvetlanaSavka/JS-44-homework-08@93e33712027ac71436bb24e3f8c4d4ad808206c4 🚀

--- 01-gallery.f4241ec9.js
+++ 01-gallery.f4241ec9.js
@@ -6,6 +6,6 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"kz9M":[function(require,module,exports) {
 
 },{}],"TfQ5":[function(require,module,exports) {
-"use strict";var e=require("./gallery-items"),i=l(require("simplelightbox"));function l(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css"),console.log(e.galleryItems);
+"use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css");const t=document.querySelector(".gallery"),i=a(e.galleryItems);function a(e){return e.map(({preview:e,original:l,description:r})=>`\n      <a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${e}" alt="${r}" />\n</a>\n`).join("")}t.insertAdjacentHTML("beforeend",i);const n=new l.default(".gallery a",{captionsData:"alt",captionDelay:250});console.log(e.galleryItems);
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/JS-44-homework-08/01-gallery.3dcc0b81.js.map
+//# sourceMappingURL=/JS-44-homework-08/01-gallery.f4241ec9.js.map
